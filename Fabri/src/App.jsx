7c0a10ca573d3e9f33583d2832e457bd56c58ce0
FVique13 <126301532+FVique13@users.noreply.github.com>
2024-05-22 22:07:27 -0300

    Add files via upload

---
+++ Fabri/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from "/src/Components/Card";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Debajo veremos un form</h1>
+      <form>
+        
+      </form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
